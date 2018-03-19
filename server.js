140bef5b8bbee3a4c7bfd887c969f760dc4a4980
Jim <jim@lokar.org>
2018-03-19 23:47:58 +0000

    Tidied up html

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const http = require("http");
 const https = require("https");
 const url = require("url");
 const fs = require("fs");
-const uuidv4 = require("uuid/v4");
 
 let connectionCount = 0;
+
+const LISTEN_PORT = 8080;
 
 const CONSUMER_KEY = "l3rsf0pa7db0mjvkpnltv48fvu";
 const CONSUMER_SECRET = "9v4i46u7okuvtuj9rkiljpnu5m";
@@ -15,7 +16,7 @@ const REDIRECT_URI_ROOT = "http://localhost:8080/";
 main();
 
 function main() {
-    console.log("Starting listen server");
+    console.log(`Starting listen server on ${REDIRECT_URI_ROOT}`);
     http.createServer((request, response) => {
 
         connectionCount++;
@@ -23,14 +24,14 @@ function main() {
 
         if (request.url.startsWith("/loggedin")) {
             handleLoggedIn(request, response);
-        }
-        else if (request.url.startsWith("/profile")) {
+        } else if (request.url.startsWith("/profile")) {
             handleProfile(request, response);
-        }
-        else {
+        } else if (request.url.startsWith("/fake")) {
+            serveFile(response, "./fake.html");
+        } else {
             handleIndex(request, response);
         }
-    }).listen(8080);
+    }).listen(LISTEN_PORT);
 }
 
 function serveFile(response, fileName, data) {
@@ -41,8 +42,7 @@ function serveFile(response, fileName, data) {
             console.log("err");
             response.writeHead(404);
             response.end(err);
-        }
-        else if (html) {
+        } else if (html) {
 
             if (data !== undefined) {
                 html = formatString(html, data);
@@ -61,10 +61,9 @@ function formatString(text, object) {
     let newText = text.toString();
     for (let key of Object.keys(object)) {
 
-        if (typeof(object[key]) === "object") {
+        if (typeof (object[key]) === "object") {
             newText = formatString(newText, object[key]);
-        }
-        else {
+        } else {
             // console.log(`[[${key}]] = ${object[key]}`);
             newText = newText.split(`[[${key}]]`).join(object[key]);
         }
@@ -72,9 +71,14 @@ function formatString(text, object) {
     return newText;
 }
 
+function generateSuid() {
+    const time = process.hrtime();
+    return `${time[0]}-${time[1]}`;
+}
+
 function handleIndex(request, response) {
-    const data = {} ;
-    data.id = uuidv4();
+    const data = {};
+    data.id = generateSuid();
     data.key = CONSUMER_KEY;
     data.secret = CONSUMER_SECRET;
     data.redirectUrl = REDIRECT_URI_ROOT;
@@ -84,22 +88,26 @@ function handleIndex(request, response) {
 
 function handleLoggedIn(request, response) {
     var query = url.parse(request.url, true).query;
-
+    query.url = request.url;
     if (query.code) {
         console.log(`User logged in via oauth, userId ${query.userId}, code ${query.code}`);
         getAccessToken(query, sendUserPage, response);
-    }
-    else {
+    } else {
         console.log(`Failed oauth, userId ${query.userId}, error ${query.error}`);
     }
-
 }
 
 function handleProfile(clientRequest, clientResponse) {
     var query = url.parse(clientRequest.url, true).query;
-    
-    let apiUri = `https://api.meetup.com/members/self?access_token=${query.access_token}`;
-    const apiRequest = https.get(apiUri, apiResponse => {
+
+    const options = {
+        protocol: "https:",
+        rejectUnauthorized: false,
+        hostname: "api.meetup.com",
+        path: `/members/self?access_token=${query.access_token}`
+    };
+
+    const apiRequest = https.get(options, apiResponse => {
 
         let data = "";
         apiResponse.on("data", (chunk) => {
@@ -120,8 +128,7 @@ function handleProfile(clientRequest, clientResponse) {
 function sendUserPage(response, data) {
     if (data == undefined) {
         // Tell user it went wrong
-    }
-    else {
+    } else {
         serveFile(response, "./loggedin.html", data);
     }
 }
@@ -132,6 +139,7 @@ function getAccessToken(query, sendUserPage, clientResponse) {
     const body = `client_id=${CONSUMER_KEY}&client_secret=${CONSUMER_SECRET}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI_ROOT}loggedin?userId=${query.userId}&code=${query.code}`;
     const options = {
         protocol: "https:",
+        rejectUnauthorized: false,
         hostname: "secure.meetup.com",
         path: "/oauth2/access",
         method: "POST",
@@ -140,10 +148,10 @@ function getAccessToken(query, sendUserPage, clientResponse) {
             "Content-Length": Buffer.byteLength(body)
         }
     };
- 
+
     const request = https.request(options, response => {
         let data = "";
-        
+
         response.on("data", (chunk) => {
             data += chunk;
         });
@@ -154,10 +162,15 @@ function getAccessToken(query, sendUserPage, clientResponse) {
             const pageData = JSON.parse(data);
             pageData.postBody = body;
             pageData.rawJson = data;
-            sendUserPage(clientResponse, pageData);
+            pageData.client_id = CONSUMER_KEY;
+            pageData.client_secret = CONSUMER_SECRET;
+            pageData.grant_type = "authorization_code";
+            pageData.redirect_uri = `${REDIRECT_URI_ROOT}loggedin?userId=${query.userId}&code=${query.code}`;
+
+            sendUserPage(clientResponse, {...pageData, ...query});
         });
     })
-    
+
     request.on("error", (err) => {
         console.log(`Access token request error: ${err.message}`);
     });
